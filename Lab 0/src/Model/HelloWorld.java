fdbe60bd4434b3386c37314d45a88cfcc4a2c8f0
Aniruddhachitte26 <chitte.a@northeastren.edu>
2024-09-12 23:13:06 -0400

    These are my first commit for Lab 0 and Lab 1

---
+++ Lab 0/src/Model/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Aniruddha
+ */
+public class HelloWorld {
+    public static void main(String[]arg){
+        System.out.println("Hello World!");
+    }
+}
